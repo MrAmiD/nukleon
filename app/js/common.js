ca8086512fb0f57ff4bc09ede18e0cc99afa0fc8
MrAmiD <nadymov2910@gmail.com>
2017-10-10 10:01:45 +0500

    layouts typeless

---
+++ app/js/common.js
@@ -0,0 +1,58 @@
+
+
+$(function() {
+
+
+    $(window).scroll(function () {
+        var st = $(this).scrollTop(),
+            slowTxt = 20,
+            slowBg = 120; // чем больше, тем медленнее
+        $('.content-mainwin').css({
+            "transform" : "translate(0%, " + st/slowTxt + "%",
+            "opacity" : 1-st/400
+        });
+        $('.parallax-main .img-paralax').css({
+            "transform" : "translate(0%, -" + st/slowBg + "%"
+        });
+
+    });
+
+    $('input[type="tel"]').mask("+7 (999) 999-9999");
+    // ===== Scroll to Top ====
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : 0                       // Scroll to top of body
+        }, 500);
+    });
+
+
+    $('#my-menu').html($('.main-menu').html());
+    var  socials = $("#my-menu").data();
+    $("#my-menu").mmenu({
+        "extensions": [
+            "effect-menu-slide"
+        ],
+        "offCanvas": {
+            "position": "right"
+        },
+        "navbar": {
+            "title": ""
+        }
+    });
+    var api = $("#my-menu").data( "mmenu" );
+    //   Hook into methods
+    api.bind( "open:finish", function() {
+        $("#menu-btn").addClass('is-active');
+    });
+    api.bind( "close:finish", function( $panel ) {
+        $("#menu-btn").removeClass('is-active');
+    });
+
+});
